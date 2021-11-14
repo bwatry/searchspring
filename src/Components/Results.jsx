582221ff6ddf082698451d703cc0ca056f41858b
Brenna <>
2021-11-14 14:37:33 -0700

    Add comments to Results

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -5,12 +5,15 @@ import { Card } from "@mui/material";
 function Results(props) {
     // Renders product data as cards and sets class for if product is on sale
     function renderProduct(product, index) {
+        // standardizes price appearance to dollars only
         let msrp = `$${parseInt(product.msrp)}`;
         let price = `$${parseInt(product.price)}`;
+        // checks if price is less than msrp
         let isOnSale = false;
         if (product.msrp > product.price) {
             isOnSale = true;
         };
+        // checks if there is a price for the item
         if (product.price === undefined) {
             price = "Out of Stock";
         }
@@ -26,6 +29,7 @@ function Results(props) {
                             </Grid>
                             <Grid container item spacing={1} justifyContent="center">
                                 <Grid item>
+                                {/* sets styling based on sale */}
                                 <p className={isOnSale ? "on-sale" : "no-sale"}>{msrp}</p>
                                 </Grid>
                                 <Grid item>
@@ -37,9 +41,15 @@ function Results(props) {
                </Grid>
     }
 
-    return <Grid container rowSpacing={4} columnSpacing={{xs:2, sm:4, md:8}} alignItems="center" justifyContent="center" className="results-section">
-            {props.results.map(renderProduct)}
-        </Grid>
+    return <Grid container 
+            rowSpacing={4} 
+            columnSpacing={{xs:2, sm:4, md:8}} 
+            alignItems="center" 
+            justifyContent="center" 
+            className="results-section"
+            >
+                {props.results.map(renderProduct)}
+            </Grid>
 }
 
 export default Results;
